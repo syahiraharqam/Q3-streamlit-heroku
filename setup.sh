7308f0372e37e5eb389af0c245d698045d055fe4
Syahirah <syahiraharqam@gmail.com>
2021-09-26 19:33:39 +0800

    remove email

--- setup.sh
+++ setup.sh
@@ -1,11 +1,6 @@
 mkdir -p ~/.streamlit/
 
 echo "\
-[general]\n\
-email = \"syahiraharqam@gmail.com"\n\
-" > ~/.streamlit/credentials.toml
-
-echo "\
 [server]\n\
 headless = truen\n\
 enableCORS=falsen\n\
